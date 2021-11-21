6bce78aa48ed23b78f504e6e97b002d430cbae8e
Travis Puryear <travispuryear678@gmail.com>
2021-11-21 14:34:34 -0500

    animejs and about page fleshed out.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,36 +1,127 @@
 import React from "react";
-
+import anime from "animejs/lib/anime.es.js";
 import ReactAnime from "react-animejs";
 const { Anime, stagger } = ReactAnime;
 
+
+
+
+
+
 function About() {
   return (
-    <div className="container.fluid d-flex">
+    <div className="container.fluid d-flex flex-wrap justify-content-center align-items-center flex-coulmns">
       <Anime
         initial={[
-            {
-                targets: ".aboutHeaderAnimation",
-                translateY: 1100,
-                duration: 4000,
-                easing:"spring"
-            },
-            {
-                targets: ".slideIn",
-                translateY: 1100,
-                easing:"spring"
-            }
-                
+          
+          {
+            targets: ".aboutHeaderAnimation",
+            translateY: 1050,
+            
+
+            easing: "spring",
+          },
+          {
+            targets: ".slideIn",
+            translateY: 1050,
+            translateX: 50,
+  
+            
+            easing: "spring",
+          },
+          
           
         ]}
+        _onUpdate={[
+          {
+            targets: ".about-show",
+            background: "#FFF",
+            skew:0,
+            duration: 20000,
+            direction:"alternate",
+            loop:true,
+            easing: 'easeInOutQuad',
+            delay: anime.stagger(10,{
+           
+              
+            }), 
+            keyframes: [
+              {
+                translateX: 0,
+                translateY: 30
+                
+              },
+              {
+                translateY: -30
+              },
+              {
+                translateY: 30
+              },
+              {
+                translateX: 0
+              },
+              {
+                translateY: -30,
+                background: "black"
+              }
+            ],
+            
+            
+          }
+        ]}
+        
       >
-        
-       
-        
-        <div className="container">
-        <h1 className="aboutHeaderAnimation">Hello, I am Travis Puryear</h1>
-        <p className="slideIn">Full-Stack Developer</p>
+        <div className="container main-hero">
+          <h1 className="aboutHeaderAnimation display-5">Hello, I am Travis Puryear</h1>
+          <h1 className="slideIn">Full-Stack Developer</h1>
         </div>
       </Anime>
+
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+      <div className="about-show"></div>
+    
+   
+     
+      
+  
+     
+     
     </div>
   );
 }
